0836499f740d0423a3b98995172433cd9eb1bb04
CorningSun <sunkangning@koal.com>
2017-01-14 11:14:18 +0800

    修复tag和attribute无法获取问题
    * 删除DefaultConfigs.DOUBAN_XML_NAMESPACE中的配置

--- src/main/java/com/dongxuexidu/douban4j/constants/DefaultConfigs.java
+++ src/main/java/com/dongxuexidu/douban4j/constants/DefaultConfigs.java
@@ -15,10 +15,6 @@ public class DefaultConfigs {
   public static final String ACCESS_TOKEN_URL = "https://www.douban.com/service/auth2/token";
   public static final String ACCESS_TOKEN_REDIRECT_URL = "http://www.dongxuexidu.com";
   
-  public static final XmlNamespaceDictionary DOUBAN_XML_NAMESPACE = new XmlNamespaceDictionary()
-          .set("", "http://www.w3.org/2005/Atom")
-          .set("db", "http://www.douban.com/xmlns/")
-          .set("gd", "http://schemas.google.com/g/2005")
-          .set("opensearch", "http://a9.com/-/spec/opensearchrss/1.0/");
+  public static final XmlNamespaceDictionary DOUBAN_XML_NAMESPACE = new XmlNamespaceDictionary();
   
 }
